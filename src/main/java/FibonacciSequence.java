83f74663924139dbfe7878ba55c03cb4ed03078c
Cheesy2137 <adrian997112999@gmail.com>
2023-12-06 20:05:24 +0100

    changes

--- src/main/java/FibonacciSequence.java
+++ src/main/java/FibonacciSequence.java
@@ -1,39 +1,34 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class FibonacciSequence {
+public abstract class FibonacciSequence {
+
     public static void main(String[] args) {
-        List<Integer> firsFibonacciNumbs = new ArrayList<>();
-        int[] fibonacciNumbs;
+        List<Integer> fibonacciNumbs = new ArrayList<>();
 
-        firsFibonacciNumbs.add(0);
-        firsFibonacciNumbs.add(1);
-        firsFibonacciNumbs.add(1);
+        fibonacciNumbs.add(Integer.valueOf(args[0]));
+        fibonacciNumbs.add(Integer.valueOf(args[1]));
+        fibonacciNumbs.add(Integer.valueOf(args[2]));
+        int size = Integer.parseInt(args[3]);
 
-        fibonacciNumbs = sequence(firsFibonacciNumbs, 15);
+        sequence(fibonacciNumbs, size);
 
         for (int oneNumber : fibonacciNumbs) {
             System.out.print(oneNumber + " ");
         }
     }
 
-    public static int[] sequence(List<Integer> firstNumbers, int size) {
-        int[] fibonacci = new int[size];
+    public static void sequence(List<Integer> fibonacci, int size) {
         int lastSum = 1;
         int lastSum1 = 1;
         int finalSum;
 
-        for (int i = 0; i < size; i++) {
-            if (i <= 2) {
-                fibonacci[i] = firstNumbers.get(i);
-            }
-            else {
-                finalSum = lastSum + lastSum1;
-                fibonacci[i] = finalSum;
-                lastSum1 = lastSum;
-                lastSum = finalSum;
-            }
+        for (int i = 3; i < size; i++) {
+            finalSum = lastSum + lastSum1;
+            fibonacci.add(finalSum);
+            lastSum1 = lastSum;
+            lastSum = finalSum;
         }
-        return fibonacci;
     }
 }
+
